42e44d6383566418a154fc13356120e823f01c13
johnfabricio <jkf15@hotmail.com>
2023-10-24 00:04:24 +0100

    criado exercicio
    Não funciona

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,1 +1,9 @@
-console.log('O console funcionou perfeitamente')
+var vel = 60.1
+console.log(`A velocidade do carro é ${vel} km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou o limete de velocidade. MULTADO!`)
+}
+else {
+    console.log(`Boa viagem!`)
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula011/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+
+console.log(`O seu país é ${pais}`)
+if(pais == 'Brasil'){
+    console.log(`Você é brasileiro`)
+}
+else{
+    console.log('Você é estrangeiro')
+}
