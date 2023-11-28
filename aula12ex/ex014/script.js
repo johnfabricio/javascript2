00adaaeceafaaca57b3d9ae3ec2387470d745fed
johnfabricio <jkf15@hotmail.com>
2023-11-28 12:38:21 +0000

    Concluido

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,26 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 12
+    msg.innerHTML = `Agora são ${hora} horas`
+    
+    if (hora >= 0 && hora < 12){
+        //Bom dia
+        img.src = 'manha.jpg'
+        document.body.style.background = '#8098b0'
+    }
+    else if (hora >= 12 && hora < 18){
+        //Boa tarde
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#ffac34'
+    }
+    else{
+        //Boa noite
+        img.src = 'noite.jpg'
+        document.body.style.background ='#0b0a2c'
+    }
+}
+
+
