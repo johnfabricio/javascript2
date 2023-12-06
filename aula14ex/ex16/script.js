25e806c945130063436bb6c49549d1fbe1190ad0
johnfabricio <jkf15@hotmail.com>
2023-12-06 13:00:00 +0000

    concluido

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,6 +1,5 @@
 
 function calcular(){
-
     var fa1 = document.getElementById('a1')
     var fan = document.getElementById('an')
     var fr = document.getElementById('r')
@@ -8,10 +7,26 @@ function calcular(){
     var an = Number(fan.value)
     var r = Number(fr.value)
     var res = window.document.querySelector('div#res')
-    
-    for (var c = a1; c<=an; c= c+r){
-        res.innerHTML += `${c} `
+
+    if (r==0){
+        window.alert('Impossível o passo ser 0(zero). Considerando passo 1')
+        r =1
     }
+    
+    if (fa1.value.length == 0 || fan.value.length == 0 || fr.value.length == 0) {
+        window.alert('ERRO! Prencha todos os dados')
+    } else
+        if (a1<an){
+            for (var c = a1; c<=an; c+=r)
+                {res.innerHTML += `${c} `
+            }
+        } else{
+            for (var c = a1; c>=an; c-=r)
+                {res.innerHTML += `${c} `
+            }
+        }
+        
 }
+
 
 
