662f374e2a838fdc9a7e701257a56b9ca15bb49c
johnfabricio <jkf15@hotmail.com>
2023-12-08 00:10:49 +0000

    concluido a tabuada

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -7,6 +7,14 @@ function calcular(){
         window.alert('ERRO! Por favor, preencha o valor')
     } else{
         var valor = Number(fvalor.value)
+        c = 1
+        res.innerHTML = ''
+        while (c <= 10){
+            var item = document.createElement('option')
+            item.text = `${valor} x ${c} = ${valor*c}`
+            res.appendChild(item)
+            c++
+        }
         
     }
 }
