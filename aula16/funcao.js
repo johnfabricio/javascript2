adf27bda2fd4f48e417a04f41b223c197b59c07b
johnfabricio <jkf15@hotmail.com>
2023-12-16 23:06:37 +0000

    feito

---
+++ aula16/funcao.js
@@ -0,0 +1,11 @@
+function parimpa(n){
+    if (n%2 == 0){
+        return 'par'
+    }
+    else{
+        return 'impar'
+    }
+}
+
+let res = parimpa(11)
+console.log(res)
---
+++ aula16/funcao02.js
@@ -0,0 +1,4 @@
+function soma(a=0,b=0){
+    return a+b
+}
+console.log(soma(7,))
